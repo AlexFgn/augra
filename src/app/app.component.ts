4e8f539d1fc0afddf529388403e90007c889c815
alexgn023@gmail.com <alexgn023@gmail.com>
2025-06-27 13:47:21 +0200

    Project IA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';  //linea 3 incluida de primeng
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule], //linea 7 añadido "ButtonModule"
+  imports: [RouterOutlet, ButtonModule, HeaderComponent, FooterComponent], //linea 7 añadido "ButtonModule"
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { ChatComponent } from './feature/chat/chat.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    //Ruta raiz que redirige automaticamente a chat
+    {path: '', redirectTo: 'chat', pathMatch: 'full'},
+
+    //Ruta /chat que renderiza ChatComponent
+    {path: 'chat', component: ChatComponent}, 
+
+    // Ruta comodin que redirige cualquier otra ruta a 'chat'
+    {path : '**', redirectTo: 'chat'}
+];
